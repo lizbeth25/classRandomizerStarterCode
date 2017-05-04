4606969727cb8647f2dfec693d1f72d79a3fa0c2
Lizbeth Bolanos Maldonado <lbolanos@aclanyc.com>
2017-05-04 02:23:09 +0000

    created class randomizer

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-
-
-
+    var students = ["Monserrat, Adonis, Maria, Leslie, David"];
+    $("#studentButton").click(function(){
+        var randomStudents = Math.floor(Math.random()* students.length);
+        $("#studentDisplay").html(students[randomStudents]);
+});
+    var teachers = ["Alyxe, Justin, Zack, Julia, Aaron"];
+    $("#teacherButton").click(function(){
+        var randomTeachers = Math.floor(Math.random()* teachers.length);
+        $("#teacherDisplay").html(teachers[randomTeachers]);
+});
 });
